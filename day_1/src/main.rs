8ac1164a53dec8de7882324f1bc604910f07c7ba
jhculb <56140853+jhculb@users.noreply.github.com>
2022-12-05 18:35:26 +0000

    Working part 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,41 +1,63 @@
-use std::env;
 use std::fs;
-use std::io::{self, prelude::*, BufReader};
-use std::path::Path;
 
-// fn diagnose_what_bloody_directory_relative_paths_are_from() {
-//     let path = env::current_dir()?;
-//     println!("The current directory is {}", path.display());
-//     for entry in fs::read_dir(".").unwrap() {
-//         let entry = entry.unwrap();
-//         let path = entry.path();
-//         if path.is_dir() {
-//             println!("{:?} is a dir", path);
-//         } else {
-//             println!("{:?} is a file", path);
-//         }
-//     }
-//     trial();
-// }
 
 fn main() {
-    trial();
+    input1();
 }
 
 fn trial() {
+    let debug = false;
     let file_path = "resource/data.txt";
-    println!("In file {}", file_path);
-    data = read_text_at_once(file_path)
-    for line in data.lines() {
-        println!("{}", line)
-    }
+    if debug {println!("In file {}", file_path);}
+    let data = read_text_at_once(file_path);
+    let answer = find_maximum(data, debug);
+    println!("Maximum calories = {}", answer);
+}
 
+fn input1() {
+    let debug = false;
+    let file_path = "resource/input1.txt";
+    if debug {println!("In file {}", file_path);}
+    let data = read_text_at_once(file_path);
+    let answer = find_maximum(data, debug);
+    println!("Maximum calories = {}", answer);
+}
+
+fn challenge2() {
+    
+}
+
+fn find_maximum(data:String, debug:bool) -> i32 {
+    let mut running_total = 0;
+    let mut max = 0;
+    for line in data.lines() {
+        if debug{println!("{}", line);}
+        if line.eq("") {
+            if debug{println!("Current Maximum = {}", max);}
+            
+            if running_total > max {
+                max = running_total;
+            }
+
+            if debug{println!("New Maximum = {}", max);}
+            
+            running_total = 0;
+        }
+        else {
+            if debug {println!("rt = {}", running_total);}
+
+            let converted_string = line.parse::<i32>().unwrap();
+            running_total = running_total + converted_string;
+
+            if debug {println!("Running total = {}\n", running_total);}
+        }
+    }
+    max
 }
 
 
-fn read_text_at_once(file_path:String) -> String {
+fn read_text_at_once(file_path:&str) -> String {
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
-    contents
+    return contents;
 }
