47fa555e6b1e24d87c5b0724d3c3ae25b8c39db2
jhculb <56140853+jhculb@users.noreply.github.com>
2022-12-05 18:52:41 +0000

    Nearly there day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,21 +2,13 @@ use std::fs;
 
 
 fn main() {
-    input1();
+    challenge2();
 }
 
-fn trial() {
-    let debug = false;
-    let file_path = "resource/data.txt";
-    if debug {println!("In file {}", file_path);}
-    let data = read_text_at_once(file_path);
-    let answer = find_maximum(data, debug);
-    println!("Maximum calories = {}", answer);
-}
 
-fn input1() {
+fn challenge1() {
     let debug = false;
-    let file_path = "resource/input1.txt";
+    let file_path = "resource/input.txt";
     if debug {println!("In file {}", file_path);}
     let data = read_text_at_once(file_path);
     let answer = find_maximum(data, debug);
@@ -24,7 +16,14 @@ fn input1() {
 }
 
 fn challenge2() {
-    
+    let debug = true;
+    let file_path = "resource/data.txt";
+    if debug {println!("In file {}", file_path);}
+    let data = read_text_at_once(file_path);
+    let answer = find_maximum2(data, debug);
+    println!("Top 3 calorie carrying gnomes = {}, {}, {}", answer[0], answer[1], answer[2]);
+    let sum = answer[0] + answer[1] + answer[2];
+    if debug {println!("Total of top = {}", sum)}
 }
 
 fn find_maximum(data:String, debug:bool) -> i32 {
@@ -55,6 +54,45 @@ fn find_maximum(data:String, debug:bool) -> i32 {
     max
 }
 
+fn find_maximum2(data:String, debug:bool) -> [i32;3] {
+    let mut running_total = 0;
+    let mut max = 0;
+    let mut max2 = 0;
+    let mut max3 = 0;
+    for line in data.lines() {
+        if debug{println!("{}", line);}
+        if line.eq("") {
+            if debug{println!("Current Top 3 = {}, {}, {}", max, max2, max3);}
+            
+            if running_total > max {
+                max3 = max2;
+                max2 = max;
+                max = running_total;
+            }
+            else if running_total > max2 {
+                max3 = max2;
+                max2 = running_total;
+            }
+            else if running_total > max3 {
+                max3 = running_total;
+            }
+            
+
+            if debug{println!("New Top 3 = {}, {}, {}", max, max2, max3);}
+            
+            running_total = 0;
+        }
+        else {
+            if debug {println!("rt = {}", running_total);}
+
+            let converted_string = line.parse::<i32>().unwrap();
+            running_total = running_total + converted_string;
+
+            if debug {println!("Running total = {}\n", running_total);}
+        }
+    }
+    [max, max2, max3]
+}
 
 fn read_text_at_once(file_path:&str) -> String {
     let contents = fs::read_to_string(file_path)
--- folder_creation.ps1
+++ folder_creation.ps1
@@ -6,10 +6,8 @@ while($counter -lt 32){
     $resource_folder = $main_folder + "\src\resource"
     New-Item -Path $resource_folder -ItemType Directory
     $data_txt = Join-Path $resource_folder -ChildPath "data.txt"
-    $input_txt = Join-Path $resource_folder -ChildPath "input1.txt"
-    $input2_txt = Join-Path $resource_folder -ChildPath "input2.txt"
+    $input_txt = Join-Path $resource_folder -ChildPath "input.txt"
     New-Item -Path $data_txt
     New-Item -Path $input_txt
-    New-Item -Path $input2_txt
     $counter += 1;
 }
